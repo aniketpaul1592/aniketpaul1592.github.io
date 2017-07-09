4f31427745c94eeb510faca9f02967f12d5ec863
AniketPaul1592 <aniketpaul1592@gmail.com>
2017-07-10 01:11:00 +0530

    implemented gulp-gh

--- js/source/socketReceiver.js
+++ js/source/socketReceiver.js
@@ -13,37 +13,35 @@ function usingSocketData(socketData){
 	for(var props of socketData){
 		var dateElem = new Date();
 		var msgTime = dateElem.getTime();
-		createTableEntry(props[0],props[1],msgTime); 
+		createTableEntry(props[0],props[1],msgTime);
+		// Method create an array and then push new entries to it.
+		createGraph(props[0],props[1],msgTime);
 	}
 }
 
 //Populates the table
 function createTableEntry(companyCode, price, msgTime){
-	//var timeInfo = getTimeInfo(time);
-
-	console.log("Company Code"+companyCode);
-	console.log("Company Price"+price);
-	console.log("CurrTime"+msgTime);
-
 	// This condition checks if the data from socket channel is a new entry or not
 	
-	if(document.getElementById(companyCode)){
+	if(document.getElementById(companyCode+"r")){
 		console.log("Yes");
-		var strHTML2 = "<td id='"+companyCode+"c'>"+companyCode.toUpperCase()+"</td>\
+		var strHTML2 = "<td id='"+companyCode+"c'><a href='http://www.nasdaq.com/symbol/"+companyCode+"' target='_blank'>"+companyCode.toUpperCase()+"</a></td>\
                         <td id='"+companyCode+"p'>"+price+"</td>\
-                        <td id='"+companyCode+"t'>"+msgTime+"</td>";
-        //Price Change finder
+                        <td class='msgTime' time='"+msgTime+"' id='"+companyCode+"t'>Just Now..</td>";
+        //Time Uppdate Call
+        maintainRealTime(msgTime);
         	var iniPrice = $('#'+companyCode+'p').html();
         	iniPrice = parseFloat(iniPrice);
         	var newPrice = parseFloat(price);
         	var diffPrice = newPrice - iniPrice;
-        	$('#'+companyCode).html(strHTML2);
+        	$('#'+companyCode+'r').html(strHTML2);
+        //Price Change finder
         	maintainColorCode(diffPrice,companyCode);
 	}else{
-		var strHTML = 	"<tr id='"+companyCode+"'>\
-                        	<td id='"+companyCode+"c'>"+companyCode.toUpperCase()+"</td>\
+		var strHTML = 	"<tr id='"+companyCode+"r'>\
+                        	<td id='"+companyCode+"c'><a href='http://www.nasdaq.com/symbol/'"+companyCode+"'>"+companyCode.toUpperCase()+"</a></td>\
                         	<td id='"+companyCode+"p'>"+price+"</td>\
-                        	<td id='"+companyCode+"t'>"+msgTime+"</td>\
+                        	<td class='msgTime' time='"+msgTime+"' id='"+companyCode+"t'>Just Now..</td>\
                     	</tr>";
 		var iniData	= $('#stocksAppTable').html();
     	$('#stocksAppTable').html(iniData+strHTML);
@@ -53,9 +51,30 @@ function createTableEntry(companyCode, price, msgTime){
 
 //function to maintain RealTime Time cal
 function maintainRealTime(msgTime){
-	setInterval(function(){
-		// Need to write the code and alternative
-	}, 60000);
+	//Updating time on every socket response Instead of using setInterval
+	$('#stocksTable .msgTime').each(function()
+	{
+	  var tableData = $(this).attr('time');
+	  var msg = "";
+	  var timeDiff = Math.ceil((msgTime - tableData)/1000); // To Nearest Seconds
+	  if(timeDiff<=(60)){
+	  	msg = "Few Secs Ago..";
+	  	$(this).html(msg);
+	  }else if((60)<timeDiff<=(60*60)){
+	  	var temp = Math.floor(timeDiff/60);
+	  	msg = temp+" mins ago";
+	  	$(this).html(msg);
+	  }else if((60*60)<timeDiff<=(24*60*60)){
+	  	var temp = Math.floor(timeDiff/3600);
+	  	msg = temp+" hours ago";
+	  	$(this).html(msg);
+	  }else if((24*60*60)<timeDiff<=(30*24*60*60)){
+	  	var temp = Math.floor(timeDiff/24*3600);
+	  	msg = temp+" days ago";
+	  	$(this).html(msg);
+	  }
+
+	});
 }
 
 //function to update colors
@@ -78,4 +97,86 @@ function maintainColorCode(diffPrice,companyCode) {
 			$('#'+companyCode+'p').addClass("rateUp");
 		}
 	}
+}
+
+function createGraph(companyCode,price,msgTime){
+	var dps = {};
+	 var map = new Map();
+	var chart = new CanvasJS.Chart(companyCode,{
+		title :{
+			text: companyCode
+		},			
+		data: [{
+			type: "line",
+			dataPoints: dps 
+		}]
+	});
+	if(!document.getElementById(companyCode)){
+ 		var strHTML =  "<div class='chartdiv' id='"+companyCode+"'></div>";
+		 var iniData  =   $('#graphLayout').html();
+
+		 $('#graphLayout').html(iniData+strHTML);
+		 var temp = []; // dataPoints
+		 
+
+		 msgTime = msgTime/1000;
+		 temp.push({msgTime,price});
+		 map.set(companyCode,temp);
+		 var dps = map.get(companyCode);
+		chart.render();
+	}else{
+		var dataLength = 500;
+		if(map.get(companyCode)){
+			msgTime = msgTime/1000;
+			var dps = map.get(companyCode);
+			
+			dps.push({
+				x: msgTime,
+				y: price
+			});
+			map.set(companyCode,dps)
+			if (dps.length > dataLength)
+			{
+				dps.shift();				
+			}
+		console.log(dps.length);
+		chart.render();
+		}
+		
+	// var xVal = 0;
+	// var yVal = 100;	
+	// var updateInterval = 100;
+	//  // number of dataPoints visible at any point
+
+	// var updateChart = function () {
+	// 	//count = count || 1;
+	// 	// count is number of times loop runs to generate random dataPoints.
+		
+	// 	// for (var j = 0; j < count; j++) {	
+	// 	// 	yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
+	// 		dps.push({
+	// 			x: msgTime,
+	// 			y: price
+	// 		});
+	// 		//xVal++;
+	// 	//};
+	// 	if (dps.length > dataLength)
+	// 	{
+	// 		dps.shift();				
+	// 	}
+	// 	console.log(dps.length);
+	// 	chart.render();		
+
+	// };
+
+	// // generates first set of dataPoints
+	// updateChart(); 
+
+	}
+	
+	
+	// update chart after specified time. 
+	//(function(){updateChart()}, updateInterval); 
+
+                    
 }
